660a7dc8aa2a7ffc82b4d4badb760e2395673c30
Trapson <me@trapson.com>
2021-06-06 12:41:12 +0100

    changed

--- js/yes.js
+++ js/yes.js
@@ -1,35 +1,35 @@
-    window.onload = function () {
+window.onload = function () {
 
-    document.addEventListener("contextmenu", function (e) {
-        e.preventDefault();
-        alert("Trapson");
-    }, false);
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+    alert("Change Me in js/yes.js");
+}, false);
     document.addEventListener("keydown", function (e) {
     //document.onkeydown = function(e) {
     // "I" key
     if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
         disabledEvent(e);
-        alert("Trapson");
+        alert("Change Me in js/yes.js");
     }
     // "J" key
     if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
         disabledEvent(e);
-        alert("Trapson");
+        alert("Change Me in js/yes.js");
     }
     // "S" key + macOS
     if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
         disabledEvent(e);
-        alert("Trapson");
+        alert("Change Me in js/yes.js");
     }
     // "U" key
     if (e.ctrlKey && e.keyCode == 85) {
         disabledEvent(e);
-        alert("Trapson");
+        alert("Change Me in js/yes.js");
     }
     // "F12" key
     if (event.keyCode == 123) {
         disabledEvent(e);
-        alert("Trapson");
+        alert("Change Me in js/yes.js");
     }
 }, false);
 function disabledEvent(e) {
@@ -38,7 +38,7 @@ function disabledEvent(e) {
     } else if (window.event) {
         window.event.cancelBubble = true;
     }
-    alert("Trapson");
+    alert("Change Me in js/yes.js");
     e.preventDefault();
     return false;
 }
